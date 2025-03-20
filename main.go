8207e3f81b6de1958757ad35b8208710565c00ad
Ilya Eryomenko <ilya@eryomenko.com>
2025-03-20 14:00:04 +0500

    Refactor project structure: remove old index.html, add new index.html in app directory, and implement health check endpoint in main.go
    Signed-off-by: Ilya Eryomenko <ilya@eryomenko.com>

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
+
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
+}
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.HandleFunc("/healthz", handleReadiness)
+
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./app/"))))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 	server.ListenAndServe()
-	
+
 }
