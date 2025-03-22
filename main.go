dd35fbdca98873a062c85cd8c8dfc15d13d41b2a
Ilya Eryomenko <ilya@eryomenko.com>
2025-03-22 15:44:08 +0500

    Add metrics middleware and endpoints for file server hits tracking
    Signed-off-by: Ilya Eryomenko <ilya@eryomenko.com>

--- main.go
+++ main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
-	"io"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(int32(1))
+		next.ServeHTTP(w, r)
+	})
+}
 
 func handleReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "OK")
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte("Hits: " + strconv.Itoa(int(hits)) + "\n"))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
+	var apiCfg apiConfig
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/healthz", handleReadiness)
-
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./app/"))))
+	appHandler := http.StripPrefix("/app/", http.FileServer(http.Dir("./app/")))
+	serveMux.Handle("/app/", apiCfg.middleWareMetricsInc(appHandler))
+	serveMux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	serveMux.HandleFunc("/reset", apiCfg.handleReset)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
